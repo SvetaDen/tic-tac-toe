1febd41774d7cae0e8cc840fe5cd8f4ba06d460e
Sveta Denisyuk <svetlana.denisyuk@gmail.com>
2024-02-25 14:08:45 +0200

    deriving state from props

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+const Player = ({initialName,symbol,isActive}) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName,setPlayerName]=useState(initialName);
+    const handleEditClick = () => {
+        setIsEditing((editing)=>!editing);
+    };
+    const handleNameChange = (e)=>{
+        setPlayerName(e.target.value)
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    // let btnCaption = 'Edit'
+    if (isEditing){
+        editablePlayerName = <input onChange={handleNameChange} type="text" required value={playerName}/>
+        // btnCaption ='Save'
+    }
+        return (
+            <li className={isActive? 'active': undefined}>
+                <span className="player">
+                   {editablePlayerName}
+                   <span className="player-symbol">{symbol}</span>
+                </span>
+                <button onClick={handleEditClick}>{isEditing? 'Save' : 'Edit'}</button>
+            </li>
+        )
+    return (
+        <input/>
+    );
+};
+
+export default Player;
